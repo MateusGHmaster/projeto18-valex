0f69cc2b36514e5b34a2f230ced5c1eecd81b712
Mateus <mateusoliveira469@gmail.com>
2022-07-13 11:26:49 -0400

    feat: payment service

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -1,2 +1,34 @@
-import * as utils from '../utils/otherUtils.js';
+import { utils } from '../utils/otherUtils.js';
 import * as paymentRepository from '../repositories/paymentRepository.js';
+import * as rechargeRepository from '../repositories/rechargeRepository.js';
+
+async function postNewPayment (businessId: number, cardId: number, password: string, amount: number) {
+    
+    const card = await utils.checkForCard(cardId);
+    await utils.checkIfActivatedCard(card.password);
+    await utils.checkIfExpiredCard(card.expirationDate);
+    await utils.checkIfBlockedCard(card);
+    await utils.checkPassword(password, card);
+    
+    const business = await utils.checkIfRegisteredBusiness(businessId);
+    await utils.checkIfBusinessLinkedToCardByType(card.type, business.type);
+
+    const payments = await paymentRepository.findByCardId(cardId);
+
+    const recharges = await rechargeRepository.findByCardId(cardId);
+
+    const balance = await utils.getCardBalance(payments, amount);
+    await utils.checkBalance(balance, amount);
+
+    const paymentInfo = { cardId, businessId, amount };
+    await paymentRepository.insert(paymentInfo);
+
+    return;
+
+}
+
+export const paymentService = {
+
+    postNewPayment
+
+};
