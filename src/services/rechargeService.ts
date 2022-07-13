9f630ddf8fd0731843d70598343dac70c8ba1162
Mateus <mateusoliveira469@gmail.com>
2022-07-13 11:42:23 -0400

    feat: recharge service

--- src/services/rechargeService.ts
+++ src/services/rechargeService.ts
@@ -1,2 +1,21 @@
-import * as utils from '../utils/otherUtils.js';
+import { utils } from '../utils/otherUtils.js';
 import * as rechargeRepository from '../repositories/rechargeRepository.js';
+
+async function cardRecharge (key: string, cardId: number, amount: number) {
+
+    await utils.checkForCompany(key);
+
+    const card = await utils.checkForCard(cardId);
+    await utils.checkIfActivatedCard(card.password);
+    await utils.checkIfExpiredCard(card.expirationDate);
+
+    const rechargeInfo = { cardId, amount };
+    await rechargeRepository.insert(rechargeInfo);
+
+}
+
+export const rechargeService = {
+
+    cardRecharge
+
+}
